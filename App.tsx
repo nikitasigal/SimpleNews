288ae07d20b2774c039b26b715f2bcc775f25d38
Nikita Sigal <nikitasigal@icloud.com>
2024-05-10 20:29:27 +0300

    Add Splash Screen and App Icons

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BootSplash from 'react-native-bootsplash';
 
 import NewsScreen from './screens/news/NewsScreen';
 import OnboardingScreen from './screens/onboarding/OnboardingScreen';
@@ -11,6 +12,10 @@ const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   const [hasFinishedOnboarding, setHasFinishedOnboarding] = useState(false);
+
+  useEffect(() => {
+    BootSplash.hide({fade: true});
+  }, []);
 
   return !hasFinishedOnboarding ? (
     <OnboardingScreen setFinished={setHasFinishedOnboarding} />
